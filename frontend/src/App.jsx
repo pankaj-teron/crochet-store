bf7c0ee2a1ea16761771b60f78ddec7270506721
Pankaj Teron <pnkj123teron@gmail.com>
2026-02-17 01:45:51 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/about";
+import Product from "./pages/Product";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,8 @@
+// 56
+import React from "react";
+
+const Hero = () => {
+  return <div className="bg-red-600 w-80 h-60 "></div>;
+};
+
+export default Hero;
